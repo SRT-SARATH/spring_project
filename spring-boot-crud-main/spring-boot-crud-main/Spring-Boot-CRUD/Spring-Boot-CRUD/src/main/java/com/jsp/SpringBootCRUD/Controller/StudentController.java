f2c09b4864406a53908c491708f471fb92cb7637
sarathkumar <157972475+SRT-SARATH@users.noreply.github.com>
2024-05-05 21:13:11 +0530

    Add files via upload

---
+++ spring-boot-crud-main/spring-boot-crud-main/Spring-Boot-CRUD/Spring-Boot-CRUD/src/main/java/com/jsp/SpringBootCRUD/Controller/StudentController.java
@@ -0,0 +1,64 @@
+package com.jsp.SpringBootCRUD.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jsp.SpringBootCRUD.Dto.ResponseStructure;
+import com.jsp.SpringBootCRUD.Dto.Student;
+import com.jsp.SpringBootCRUD.Service.StudentService;
+
+@RestController
+public class StudentController {
+	
+	@Autowired
+	StudentService studentService;
+	
+	// save student
+	@PostMapping("/student")
+	public ResponseStructure<Student> saveStudent(@RequestBody Student student){
+		return studentService.saveStudent(student);
+	}
+	
+	@PostMapping("student/save")
+	public Student saveStudentRecord(@RequestBody Student student) {
+		return studentService.saveStudentRecord(student);
+	}
+	// get student by id
+	@GetMapping("/student/{id}")
+	public ResponseStructure<Student> getStudentById(@PathVariable int id){
+		return studentService.getStudentById(id);
+	}
+	
+	
+	@GetMapping("student/history")
+	public List<Student> getAllStudent(){
+		return studentService.getAllStudent();
+	}
+	
+	// get all students
+	@GetMapping("/student")
+	public List<Student> getallStudent(){
+		return studentService.getAllStudent();
+	}
+	
+	// update student
+	@PutMapping("/student/{id}")
+	public ResponseStructure<Student> updateStudent(@RequestBody Student student, @PathVariable Integer id){
+		return studentService.updateStudent(student, id);
+	}
+	
+	// delete student
+	@DeleteMapping("/student/{id}")
+	public ResponseStructure<String> deleteStudent(@PathVariable int id){
+		return studentService.deleteStudent(id);
+	}
+}
